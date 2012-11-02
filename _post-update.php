707a8c143c0efb7716f29c12665971062dd455c7
Andrew Fox <andrew@andrewfox.co.uk>
2012-11-02 15:04:00 +0000

    Small
    1. Add Dom to email list
    2. Added fonts.com

--- _post-update.php
+++ _post-update.php
@@ -2,5 +2,6 @@
 
 	`git pull origin master`;
 	mail('andrew@livework.co.uk', 'Livework dev site updated', 'Just like that');
+	mail('dom@livework.co.uk', 'Livework dev site updated', 'Just like that');
 
 ?>
--- themes/livework/header.php
+++ themes/livework/header.php
@@ -34,24 +34,15 @@
 		<script src="<?php echo bloginfo('template_directory'); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
 		<script src="<?php bloginfo( 'template_directory' ); ?>/js/main.js"></script>
 
-		<!-- Symbolset social -->
-		<link href="<?php echo bloginfo('template_directory'); ?>/css/ss-social/ss-social.css" rel="stylesheet" />
-		
-		
-<?php
-		/* We add some JavaScript to pages with the comment form
-		 * to support sites with threaded comments (when in use).
-		 */
-		if ( is_singular() && get_option( 'thread_comments' ) )
-			wp_enqueue_script( 'comment-reply' );
+		<!-- Fonts.com -->
+		<script type="text/javascript" src="http://fast.fonts.com/jsapi/0fd39f14-ee12-405d-90c4-2bbbdae75f52.js"></script>
 
-		/* Always have wp_head() just before the closing </head>
-		 * tag of your theme, or you will break many plugins, which
-		 * generally use this hook to add elements to <head> such
-		 * as styles, scripts, and meta tags.
-		 */
-		wp_head();
-?>
+		<!-- Symbolset social -->
+		<link href="<?php echo bloginfo('template_directory'); ?>/webfonts/ss-standard.css" rel="stylesheet" />
+		<link href="<?php echo bloginfo('template_directory'); ?>/webfonts/ss-social.css" rel="stylesheet" />
+
+		<?php wp_head(); ?>
+
 	</head>
 	<body <?php body_class(); ?>>
 		<header role="banner">
