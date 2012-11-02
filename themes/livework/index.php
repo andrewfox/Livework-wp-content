3a778df968d25972c60909eac3a70d5c61403010
Andrew Fox <andrew@andrewfox.co.uk>
2012-11-02 16:47:35 +0000

    Update to index.php

--- themes/livework/index.php
+++ themes/livework/index.php
@@ -15,13 +15,52 @@
 
 get_header(); ?>
 
-			<?php
-			/* Run the loop to output the posts.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-index.php and that will be used instead.
-			 */
-			 get_template_part( 'loop', 'index' );
-			?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<?php get_sidebar(); ?>
+					<h1 class="page-title">News</h1>
+
+					<?php while ( have_posts() ) : the_post() ?>
+					
+						<div id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
+							
+							<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+
+
+							<?php if ( has_post_thumbnail() ) {
+								echo '<div class="post-image">';
+								the_post_thumbnail('medium');
+								echo '</div>';
+							} ?>			
+
+							<div class="entry-content">
+								
+								<div class="entry-meta">
+									<span class="meta-prep meta-prep-author"><?php _e('By ', 'blankslate'); ?></span>
+									<span class="author vcard"><a class="url fn n" href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" title="<?php printf( __( 'View all articles by %s', 'blankslate' ), $authordata->display_name ); ?>"><?php the_author(); ?></a></span>
+									<span class="meta-sep"> | </span>
+									<span class="meta-prep meta-prep-entry-date"><?php _e('Published ', 'blankslate'); ?></span>
+									<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
+									<?php edit_post_link( __( 'Edit', 'blankslate' ), "<span class=\"meta-sep\"> | </span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>
+								</div>
+
+								<?php the_excerpt(); ?>
+							</div>
+
+						</div><!-- /.post -->
+					
+					
+					<?php endwhile; ?>
+
+						<div class="nav-paged">
+							<p class="nav-previous"><?php next_posts_link(__( '<span class="meta-nav">&laquo;</span> older articles', 'blankslate' )) ?></p>
+							<p class="nav-next"><?php previous_posts_link(__( 'newer articles <span class="meta-nav">&raquo;</span>', 'blankslate' )) ?></p>
+						</div>
+					
+					<?php } ?>
+
+				</article><!-- /#bodytext -->
+
+
+
+
 <?php get_footer(); ?>
