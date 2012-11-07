39f0469998424964fdb10443266eeee24698a295
Andrew Fox <andrew@andrewfox.co.uk>
2012-11-07 07:30:23 +0000

    Symbolset fixes

--- themes/livework/page-story.php
+++ themes/livework/page-story.php
@@ -62,8 +62,8 @@ get_header(); ?>
 
 
 				<div id="more-info">
-					<h2><a id = "more-infolink" href="#"><span class="ss-standard">arrow</span> More on <?php the_title(); ?></a></h2>
-					<h1><?php the_field('page_title_suffix'); ?></h1>
+					<h2><a id = "more-infolink" href="#"><span class="ss-icon">down</span> More on <?php the_title(); ?></a></h2>
+					<h1><?php the_title(); ?>: <span><?php the_field('page_title_suffix'); ?></span></h1>
 				</div>
 
 
