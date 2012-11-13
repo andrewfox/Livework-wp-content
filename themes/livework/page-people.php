6583795477859e491577fa3714811ce07d1501c5
Andrew Fox <andrew@andrewfox.co.uk>
2012-11-13 16:51:17 +0000

    .

--- themes/livework/page-people.php
+++ themes/livework/page-people.php
@@ -16,35 +16,41 @@ get_header(); ?>
 					<h1 class="page-title"><?php the_title(); ?></h1>
 
 					<div class="entry-content">
-						<ul>
-							<?php the_content(); ?>
-							<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
-							<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
-						</div><!-- .entry-content -->
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
+					</div><!-- .entry-content -->
 						
-						<?php query_posts(array('post_type' => 'people', 'posts_per_page' => 100 , 'order' => 'DSC', 'paged'=> $paged)); ?>
-			
-						<?php while(have_posts()) : the_post(); 	
-	
-							echo '<li>';
-							
-							the_title();
-							
-							
-							if(has_post_thumbnail()) :
-							the_post_thumbnail('original'); 
-							else :				
-	
-							endif;
-							
-							echo '</li>';
-							
-							endwhile; ?>
+					<ul id="offices">
+						<li class="london">London</li>
+						<li class="oslo">Oslo</li>
+						<li class="sao-paulo">Sao Paulo</li>
+						<li class="rotterdam">Rotterdam</li>
+					</ul>
+					
+					<ul id="people">
+					<?php query_posts(array('post_type' => 'people', 'posts_per_page' => 100 , 'order' => 'DSC', 'paged'=> $paged)); ?>
+		
+					<?php while(have_posts()) : the_post();  ?>
+
+						<li><a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
+							<?php the_title(); 
 						
-						</ul>
-						</div>
+						
+						if(has_post_thumbnail()) :
+						the_post_thumbnail('original'); 
+						else :				
+
+						endif;
+						
+						?>
+						</a></li>
+						
+						<?php endwhile; ?>
+					
+					</ul>
 	
-						<?php wp_reset_query();?>
+					<?php wp_reset_query();?>
 
 				</article><!-- #page-## -->
 
