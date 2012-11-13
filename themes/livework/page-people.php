c749a37fe70902d2d1e3fffe64bae5bb1c76ce7e
Dom <dom@livework.co.uk>
2012-11-13 16:14:43 +0000

    people page

--- themes/livework/page-people.php
+++ themes/livework/page-people.php
@@ -16,52 +16,35 @@ get_header(); ?>
 					<h1 class="page-title"><?php the_title(); ?></h1>
 
 					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
-						<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
-					</div><!-- .entry-content -->
-					
-					<?php query_posts(array('post_type' => 'people', 'posts_per_page' => 100 , 'order' => 'DSC', 'paged'=> $paged)); ?>
-		
-					<?php while(have_posts()) : the_post(); ?>	
-
-					<div class="person">
+						<ul>
+							<?php the_content(); ?>
+							<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
+							<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
+						</div><!-- .entry-content -->
 						
-						<!-- VIDEOS VIA CUSTOM FIELD VALUE-->	
-						<?php  if(has_post_thumbnail()) :?>
-						<?php the_post_thumbnail('original'); ?>
-						<?php else :?>
-												
-						<a href="<?php the_field('video_url'); ?>" 
-						
-							
-								<?php
-								 /* if no url spcified refresh videopage*/
-								if(get_field('video_url'))
-								{
-									echo 'class="iframe"';
-								}?>>
-							<img src="<?php echo bloginfo('template_url'); ?>/img/default_thumbnail.jpg" alt="default_thumb" />
-							
-						
-
-						<?php endif;?>
-						<span class="play-icon"></span>
-						<span class="video-text"><?php the_title(); ?><span class="video-people-text"><?php the_field('people'); ?></span></span>
-						
-
-						<!-- END VIDEOS VIA CUSTOM FIELD VALUE -->
-
-						</a>
-						
-
-					</div><!-- /.video -->
+						<?php query_posts(array('post_type' => 'people', 'posts_per_page' => 100 , 'order' => 'DSC', 'paged'=> $paged)); ?>
+			
+						<?php while(have_posts()) : the_post(); 	
 	
-
-
-					<?php endwhile; ?>
-
-					<?php wp_reset_query();?>
+							echo '<li>';
+							
+							the_title();
+							
+							
+							if(has_post_thumbnail()) :
+							the_post_thumbnail('original'); 
+							else :				
+	
+							endif;
+							
+							echo '</li>';
+							
+							endwhile; ?>
+						
+						</ul>
+						</div>
+	
+						<?php wp_reset_query();?>
 
 				</article><!-- #page-## -->
 
