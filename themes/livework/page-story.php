024eaea6b1affd921b4dfa443e10a7cd2b10bcb5
Dom <dom@livework.co.uk>
2012-11-14 11:48:14 +0000

    .

--- themes/livework/page-story.php
+++ themes/livework/page-story.php
@@ -89,7 +89,9 @@ get_header(); ?>
 					 		
 					 			$layoutvar = the_sub_field("section_layout");
 					 			
-					 			echo '<div class="'$layoutvar ' story-section">'
+					 			echo '<div class="'
+					 			echo $layoutvar
+					 			echo ' story-section">'
 					 											
 					 		?>
 								 							
