edf13a5452dae7b2a78ced2b8cbf842157a446e7
Dom <dom@livework.co.uk>
2012-11-16 15:28:58 +0000

    .

--- themes/livework/single-people.php
+++ themes/livework/single-people.php
@@ -28,7 +28,7 @@ get_header(); ?>
 				<?php endif; ?>
 				
 
-					    	<a href="<?php next_post_link('%link', '', '', ''); ?>"><img class="right-arrow" src="<?php bloginfo( 'template_directory' ); ?>/img/right-arrow.png" alt="go to next page" /></a>
+					    	<a href="<?php next_post_link('', '', '', ''); ?>"><img class="right-arrow" src="<?php bloginfo( 'template_directory' ); ?>/img/right-arrow.png" alt="go to next page" /></a>
 				
 				</div>
 				
