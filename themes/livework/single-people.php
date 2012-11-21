2c88da4bd78a9a0e5c5ef0bde07b669172734ec7
Dom <dom@livework.co.uk>
2012-11-21 15:59:49 +0000

    .

--- themes/livework/single-people.php
+++ themes/livework/single-people.php
@@ -14,6 +14,10 @@ get_header(); ?>
 		<?php the_post_thumbnail('full'); ?>
 		<div id="person-headline" >
 			<div class="wrapper">
+				<h4><?php
+				$parent_title = get_the_title($post->post_parent);
+				echo $parent_title;
+				?></h4>
 				<h3><?php the_title(); ?>: <span><?php the_field('job_title'); ?></span></h3>
 				<h2><?php the_excerpt() ?></h2>
 			</div>
