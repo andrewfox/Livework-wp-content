965698d0ecb38be8e1f0e6e86ffe562fb346334d
Dom <dom@livework.co.uk>
2012-11-22 17:43:56 +0000

    `

--- themes/livework/single-case_study.php
+++ themes/livework/single-case_study.php
@@ -52,7 +52,7 @@ get_header(); ?>
 			
 			    <?php $args = array( 
 			    					'author'=> $authorid,
-//			    					'post_type' => 'people', 
+			    					'post_type' => 'people', 
 			    					'posts_per_page' => 1, 
 			    					
 			    					);
