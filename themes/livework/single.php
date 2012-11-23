fd716a84b79a1e8d28378e7843e44e8a0bba9e3c
Dom <dom@livework.co.uk>
2012-11-23 15:35:04 +0000

    .

--- themes/livework/single.php
+++ themes/livework/single.php
@@ -36,7 +36,7 @@ $separator = ' ';
 $output = '';
 if($categories){
 	foreach($categories as $category) {
-		$output .= '<a href="'.get_category_link($category->term_id ).'" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name.'</a>'.$separator;
+		$output .= '<p>'.$category->cat_name.'</p>'.$separator;
 	}
 echo trim($output, $separator);
 }
