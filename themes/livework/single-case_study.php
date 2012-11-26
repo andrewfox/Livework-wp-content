f713b4b5a506ce88c6d60c91d487267443d1d640
Dom <dom@livework.co.uk>
2012-11-26 17:26:52 +0000

    .

--- themes/livework/single-case_study.php
+++ themes/livework/single-case_study.php
@@ -91,7 +91,7 @@ get_header(); ?>
 										
 										
 										<?php
-										$terms = get_the_terms( $post->ID, 'taxonomy sectors' );
+										$terms = get_the_terms( $post->ID, 'taxonomy Sectors' );
 																
 										
 											$sectors_terms = array();
