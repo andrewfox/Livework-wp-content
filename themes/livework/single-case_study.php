5d4459e14bf9b542f67bd7d0c552c13618976217
Dom <dom@livework.co.uk>
2012-11-27 15:27:10 +0000

    single case study

--- themes/livework/single-case_study.php
+++ themes/livework/single-case_study.php
@@ -124,7 +124,7 @@ get_header(); ?>
 										
 										$query = new WP_Query( array( 
 										'sectors' => 'on_draught',
-										'posts_per_page' => -1 ,
+										'posts_per_page' => 10 ,
 										'orderby' => 'title', 
 										'order' => 'ASC', 
 										'post_type' => 'case_study',
