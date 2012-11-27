79bba9354f05daa5548c468fbeb1a13270468ad5
Andrew Fox <andrew@andrewfox.co.uk>
2012-11-27 17:21:57 +0000

    News update

--- themes/livework/index.php
+++ themes/livework/index.php
@@ -16,55 +16,48 @@
 get_header(); ?>
 
 				<article class="main">
-<div class="wrapper">
-					
+					<div class="wrapper">
 
-					<?php while ( have_posts() ) : the_post() ?>
+						<h1 class="page-title">News</h1>
+
+
+						<?php while ( have_posts() ) : the_post() ?>
 					
-						<div class="post-wrapper" id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
-							<h1 class="page-title">News</h1>
+							<div id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> class="post">
 							
-							<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+								<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
-
-										
-
-							<div class="entry-content">
 								
 								<div class="entry-meta">
-									<?php $authorid = get_the_author_meta('ID') ?> 
-									    <?php $args = array( 
-									    					'author'=> $authorid,
-									    					'post_type' => 'people', 
-									    					'posts_per_page' => 1, 
-									    					
-									    					);
-									    $loop = new WP_Query( $args );
-									    while ( $loop->have_posts() ) : $loop->the_post(); ?>
-									        <li>
-									            <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>">
-									            <?php the_post_thumbnail('thumbnail'); ?>
-									            <?php the_title(); ?>									
-									    <?php endwhile; ?>
-										<?php wp_reset_query();?>
-									    
-									<span class="meta-sep"> | </span>
-									<span class="meta-prep meta-prep-entry-date"><?php _e('Published ', 'blankslate'); ?></span>
-									<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
+								<?php $authorid = get_the_author_meta('ID') ?> 
+									<?php $args = array( 
+											'author'=> $authorid,
+											'post_type' => 'people', 
+											'posts_per_page' => 1, 
+										);
+										$loop = new WP_Query( $args );
+										while ( $loop->have_posts() ) : $loop->the_post(); ?>
+									<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>">
+											<?php the_post_thumbnail('thumbnail'); ?>
+											<?php the_title(); ?>									
+											<?php endwhile; ?>
+									<?php wp_reset_query();?>
+										<span class="meta-sep"> | </span>
+										<span class="meta-prep meta-prep-entry-date"><?php _e('Published ', 'blankslate'); ?></span>
+										<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
 									<?php edit_post_link( __( 'Edit', 'blankslate' ), "<span class=\"meta-sep\"> | </span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>
 									</a>
-									</li>
-									
 								</div>
 								
 								<?php if ( has_post_thumbnail() ) {
 									echo '<div class="post-image blog-image">';
-									the_post_thumbnail('full');
+									the_post_thumbnail('large');
 									echo '</div>';
 								} ?>
-
-								<?php the_content(); ?>
-							</div>
+									<div class="entry-content">
+										<?php the_content(); ?>
+									</div>
+								</div>
 
 						</div><!-- /.post -->
 					
