2e453357e6bdd102151ca02484f97f61700e9881
Dom <dom@livework.co.uk>
2012-11-28 15:21:59 +0000

    .

--- themes/livework/page-story-parent.php
+++ themes/livework/page-story-parent.php
@@ -19,7 +19,9 @@ get_header(); ?>
 
 						<div class="no-bkg hat" ></div>
 
-						<div class="no-bkg box" >
+						<div class="" >
+							
+							
 							<?php
 							
 							
@@ -46,13 +48,7 @@ get_header(); ?>
 							
 							
 							
-							<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-							<h3><?php the_title(); ?>: <span><?php the_field('story_page_title_suffix'); ?></span></h3>
-							<h2><?php the_field('story_page_headline'); ?></h2>
-							<div class="entry-content">
-								<?php the_content(); ?>
-							</div>
-							<?php endwhile; ?>
+							
 						</div>
 
 					</div> <!-- /#top -->
