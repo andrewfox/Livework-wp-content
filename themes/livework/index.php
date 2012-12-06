e4f7c87fa9b709c98b37fcdf67d7c946f7f96b04
Dom <dom@livework.co.uk>
2012-12-06 10:59:59 +0000

    .

--- themes/livework/index.php
+++ themes/livework/index.php
@@ -38,6 +38,11 @@ get_header(); ?>
 								<?php the_excerpt(); ?>
 							</div>
 							
+							
+								<span class="meta-prep meta-prep-entry-date"><?php _e('Published ', 'blankslate'); ?></span>
+								<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
+							<?php edit_post_link( __( 'Edit', 'blankslate' ), "<span class=\"meta-sep\"> | </span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>
+							<span class="meta-sep"> | </span>
 							<div class="entry-meta">
 								<?php 
 								$authorid = get_the_author_meta('ID');
@@ -54,10 +59,7 @@ get_header(); ?>
 										<?php the_title(); ?>									
 										<?php endwhile; ?></a>
 								<?php wp_reset_query();?>
-									<span class="meta-sep"> | </span>
-									<span class="meta-prep meta-prep-entry-date"><?php _e('Published ', 'blankslate'); ?></span>
-									<span class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php the_time( get_option( 'date_format' ) ); ?></abbr></span>
-								<?php edit_post_link( __( 'Edit', 'blankslate' ), "<span class=\"meta-sep\"> | </span>\n\t\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t" ) ?>
+									
 								
 							</div>
 							
