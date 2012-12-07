6d07e779c211a72de4bb3ff5f8aa55430dd07443
Dom <dom@livework.co.uk>
2012-12-07 16:05:32 +0000

    .

--- themes/livework/page-studio.php
+++ themes/livework/page-studio.php
@@ -36,38 +36,6 @@ get_header(); ?>
 
 				</article><!-- #page-## -->
 
-
-				<ul id="offices">
-					<li class="all on">All</li>
-					<li class="london">London</li>
-					<li class="oslo">Oslo</li>
-					<li class="sao-paulo">S&atilde;o Paulo</li>
-					<li class="rotterdam">Rotterdam</li>
-				</ul>
-
-
-				<ul id="people" class="clearfix">
-
-				<?php query_posts(array('post_type' => 'people', 'posts_per_page' => -1 , 'order' => 'ASC', 'orderby' => 'title', 'paged'=> $paged)); ?>
-	
-				<?php while(have_posts()) : the_post();  ?>
-
-					<li <?php post_class(); ?>>
-						<a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
-							<?php if(has_post_thumbnail()) :
-							the_post_thumbnail('thumb-large'); 
-							endif;?>
-							<span><?php the_title(); ?></span>
-						</a>
-					</li>
-
-					<?php endwhile; ?>
-
-				</ul>
-
-				<?php wp_reset_query();?>
-
-
 <?php endwhile; ?>
 
 <?php get_sidebar(); ?>
