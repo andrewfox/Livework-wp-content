d9247d6e813ace192ae40a47654da75155845770
Dom <dom@livework.co.uk>
2012-12-10 10:39:08 +0000

    .

--- themes/livework/page-contact_us.php
+++ themes/livework/page-contact_us.php
@@ -13,6 +13,7 @@ get_header(); ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 	<?php the_title(); ?>
 	<?php the_post_thumbnail('large'); ?>
+	<?php the_content(); ?>
 <?php endwhile; ?>
 		<div class="wrapper">
 			
