70cd2e692d6237872e258e54b4760e235eadd676
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-10 11:32:15 +0000

    Our Story fix

--- themes/livework/page-story-parent.php
+++ themes/livework/page-story-parent.php
@@ -9,40 +9,49 @@
  */
 
 get_header(); ?>
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-
-
-
-				<div class="splash">
+				<article id="page-<?php the_ID(); ?>" class="main clearfix">
+					
+					<div class="wrapper">
+				
+						<h1 class="page-title"><?php the_title(); ?></h1>
 	
-					<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-					<?php the_post_thumbnail('large'); ?>
-					<div id="introduction" >
-						<div class="wrapper">
-							<ul>
-							<?php
-
-								$mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'asc' ) );
-							
-								foreach( $mypages as $page ) {		
-									$content = $page->post_content;
-									if ( ! $content ) // Check for empty page
-										continue;
-							
-									$content = apply_filters( 'the_excerpt', $content );
-								?>
-										<li>
-											<h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
-											<div class="entry"><?php echo $content; ?></div>
-										</li>
-								<?php
-								}	
-							?>
-							</ul>
-						</div>
+						<div class="entry-content">
+							<?php the_content(); ?>
+							<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
+							<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
+						</div><!-- .entry-content -->
+					
 					</div>
-					<?php endwhile; ?>
-				</div>
+
+					<ul>
+					<?php
+
+						$mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'asc' ) );
+					
+						foreach( $mypages as $page ) {		
+							$content = $page->post_content;
+							if ( ! $content ) // Check for empty page
+								continue;
+					
+							$content = apply_filters( 'the_excerpt', $content );
+						?>
+								<li>
+									<h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
+									<div class="entry"><?php echo $content; ?></div>
+								</li>
+						<?php
+						}	
+					?>
+					</ul>
+
+
+				</article><!-- #page-## -->
+
+<?php endwhile; ?>
+
+
 
 
 				<div id="hello" class="arrows">
