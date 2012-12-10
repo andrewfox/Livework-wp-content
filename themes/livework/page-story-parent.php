33278e7c6f1be96a51dcdcb1beeb890027470d6d
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-10 11:35:31 +0000

    .

--- themes/livework/page-story-parent.php
+++ themes/livework/page-story-parent.php
@@ -22,30 +22,31 @@ get_header(); ?>
 							<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
 							<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
 						</div><!-- .entry-content -->
-					
-					</div>
 
-					<ul>
-					<?php
-
-						$mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'asc' ) );
-					
-						foreach( $mypages as $page ) {		
-							$content = $page->post_content;
-							if ( ! $content ) // Check for empty page
-								continue;
-					
-							$content = apply_filters( 'the_excerpt', $content );
-						?>
-								<li>
-									<h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
-									<div class="entry"><?php echo $content; ?></div>
-								</li>
+						<ul>
 						<?php
-						}	
-					?>
-					</ul>
+	
+							$mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'asc' ) );
+						
+							foreach( $mypages as $page ) {		
+								$content = $page->post_content;
+								if ( ! $content ) // Check for empty page
+									continue;
+									$content = apply_filters( 'the_excerpt', $content );
+							?>
+									<li>
+										<h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
+										<div class="entry">
+											<?php the_field('story_page_headline'); ?>
+										</div>
+									</li>
+							<?php
+							}	
+						?>
 
+						</ul>
+
+					</div><!-- /.wrapper -->
 
 				</article><!-- #page-## -->
 
