8f376907f0adf232b1d8d000f56fc610b3278e23
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-10 11:06:54 +0000

    .

--- themes/livework/sidebar-sectorsclients.php
+++ themes/livework/sidebar-sectorsclients.php
@@ -13,24 +13,25 @@
 					
 					<div class="wrapper">
 
-						<ul>
 						<?php
 						$sectors = get_terms( 'sectors', 'orderby=count&hide_empty=0' ); 
 						foreach ($sectors as $sector) { 
 							if ($sector->count > 0) { ?>
-							<li><a href="<?php bloginfo('url'); ?>/our-clients-stories/<?php echo $sector->slug ?>"><?php echo $sector->name ?></a>
-								<ul>
+							<ul>
+								<li><a href="<?php bloginfo('url'); ?>/our-clients-stories/<?php echo $sector->slug ?>"><?php echo $sector->name ?></a>
+									<ul>
 							<?php 
 								$clients = get_terms( 'clients', 'orderby=count&hide_empty=0' );
 								foreach ($clients as $client) { ?>
-									<li><a href="<?php bloginfo('url'); ?>/case-study/<?php echo $client->slug ?>"><?php echo $client->name ?></a></li>
+										<li><a href="<?php bloginfo('url'); ?>/case-study/<?php echo $client->slug ?>"><?php echo $client->name ?></a></li>
 								
 								<?php } ?>
-								</ul>
-							</li>
+									</ul>
+								</li>
+							</ul>
 							<?php } ?>
 						<?php } ?>
-						</ul>
+						
 
 					</div><!-- /.wrapper -->
 
