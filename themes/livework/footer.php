c406efccb835a1ce21f10bd72a87b4a6e42e55dc
Dom <dom@livework.co.uk>
2012-12-11 14:47:08 +0000

    contact in footer

--- themes/livework/footer.php
+++ themes/livework/footer.php
@@ -46,6 +46,21 @@
 		endif; ?>
 
 
+<?php query_posts(array('showposts' => 4, 'post_parent' => 15, 'post_type' => 'page', 'order' => 'asc', )); while (have_posts()) { the_post(); ?>
+
+<div class="office">
+		
+		<h2><a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+		<?php the_post_thumbnail('thumb-large');  ?>
+		<p class="studio-address"><?php the_field('studio_address'); ?></p>
+		<p class="studio-telephone"><?php the_field('studio_telephone'); ?></p>
+		<p class="studio-fax"><?php the_field('studio_fax'); ?></p>
+		<p class="studio-email"><?php the_field('studio_email'); ?></p>
+		<p class="studio-twitter"><?php the_field('studio_twitter'); ?></p>
+		<p class="studio-facebook"><?php the_field('studio_facebook'); ?></p>
+	
+</div>
+
 
 <?php
 	/* Always have wp_footer() just before the closing </body>
