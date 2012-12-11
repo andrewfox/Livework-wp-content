67c73cc78a6458a0204ef924c96694f98735db41
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-11 22:44:54 +0000

    .

--- themes/livework/sidebar-sectorsclients.php
+++ themes/livework/sidebar-sectorsclients.php
@@ -21,21 +21,19 @@
 								<li><a href="<?php bloginfo('url'); ?>/our-clients-stories/<?php echo $sector->slug ?>"><?php echo $sector->name ?></a>
 									<ul>
 																
-								<?php 
-								$sector_slug = $sector->slug;
-								query_posts(array('post_type' => 'case_study', 'sectors' => $sector_slug, 'posts_per_page' => -1 , 'order' => 'ASC', 'orderby' => 'title', 'paged'=> $paged));
-				while(have_posts()) : the_post();  ?>
+										<?php 
+										$sector_slug = $sector->slug;
+										query_posts(array('post_type' => 'case_study', 'sectors' => $sector->slug, 'posts_per_page' => -1 , 'order' => 'ASC', 'orderby' => 'title', 'paged'=> $paged));
+										while(have_posts()) : the_post();  ?>
 
-					<li <?php post_class(); ?>>
-						<a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
-							<?php the_title(); ?>
-						</a>
-					</li>
+										<li>
+											<a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
+												<?php the_title(); ?> <?php echo $sector->slug ?>
+											</a>
+										</li>
 
-					<?php endwhile; ?>
+										<?php endwhile; ?>
 
-								
-								
 									</ul>
 								</li>
 							</ul>
