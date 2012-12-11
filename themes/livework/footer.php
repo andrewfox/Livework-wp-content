cc1d649a6a79a74245f1629f73b37dcd2b34edae
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-11 10:44:04 +0000

    Remove hello arrows from page-story, add them to footer (so can work everywhere)

--- themes/livework/footer.php
+++ themes/livework/footer.php
@@ -22,6 +22,34 @@
 			</nav>
 
 		</footer><!-- footer -->
+
+
+		<div id="hello" class="arrows">
+		
+		<?php
+		$posts3 = get_field('previous_page');
+		if( get_field('previous_page') ): ?>
+		<?php foreach( $posts3 as $post): // variable must be called $post (IMPORTANT) 
+		setup_postdata($post); ?>
+		<a href="<?php the_permalink(); ?>"><img class="left-arrow" src="<?php bloginfo( 'template_directory' ); ?>/img/left-arrow.png" alt="go to previous page" /></a>
+		<?php endforeach; 
+		wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly 
+		endif; ?>
+		
+		<?php
+		$posts2 = get_field('next_page');
+		if( $posts2 ): 
+		foreach( $posts2 as $post): // variable must be called $post (IMPORTANT) ?>
+		<?php setup_postdata($post); ?>
+		<a href="<?php the_permalink(); ?>"><img class="right-arrow" src="<?php bloginfo( 'template_directory' ); ?>/img/right-arrow.png" alt="go to next page" /></a>
+		<?php endforeach; 
+		wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly 
+		endif; ?>
+		
+		</div>
+
+
+
 <?php
 	/* Always have wp_footer() just before the closing </body>
 	 * tag of your theme, or you will break many plugins, which
@@ -30,8 +58,6 @@
 	wp_footer();
 ?>
 
-
-
-
 	</body>
+
 </html>
