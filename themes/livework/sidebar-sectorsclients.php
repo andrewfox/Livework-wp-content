53f6010984094137af9d7b92c80f5dec8c7b5ae0
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-12 07:45:17 +0000

    .

--- themes/livework/sidebar-sectorsclients.php
+++ themes/livework/sidebar-sectorsclients.php
@@ -35,17 +35,37 @@
 										<?php endwhile; ?>
 										
 										
-										<?php 
-										$query = new WP_Query( 'post_type=case_study', array( 'sectors' => 'financial' ) ); 
-										if( $query->have_posts() ) {
-											echo 'List of '.$post_type . ' where the taxonomy '. $tax . '  is '. $tax_term->name;
-											while ($query->have_posts()) : $query->the_post(); ?>
-												<p><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
-											<?php endwhile;
-										}
-										wp_reset_query();
-										?>
 										
+										
+										
+										<?php
+//for a given post type, return all
+$post_type = 'case_study';
+$tax = 'sectors';
+$tax_terms = get_terms($tax);
+if ($tax_terms) {
+  foreach ($tax_terms  as $tax_term) {
+    $args=array(
+      'post_type' => $post_type,
+      "$tax" => $tax_term->slug,
+      'post_status' => 'publish',
+      'posts_per_page' => -1,
+      'caller_get_posts'=> 1
+    );
+
+    $my_query = null;
+    $my_query = new WP_Query($args);
+    if( $my_query->have_posts() ) {
+      echo 'List of '.$post_type . ' where the taxonomy '. $tax . '  is '. $tax_term->name;
+      while ($my_query->have_posts()) : $my_query->the_post(); ?>
+        <p><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
+        <?php
+      endwhile;
+    }
+    wp_reset_query();
+  }
+}
+?>
 										
 
 									</ul>
