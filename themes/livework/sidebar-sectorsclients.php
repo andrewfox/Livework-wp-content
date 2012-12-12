cc837e2d4efb0af22995d1ffd4d1305036acbc54
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-12 07:01:06 +0000

    Nope that didn't work… continuing

--- themes/livework/sidebar-sectorsclients.php
+++ themes/livework/sidebar-sectorsclients.php
@@ -23,12 +23,12 @@
 																
 										<?php 
 										$sector_slug = $sector->slug;
-										query_posts(array('post_type' => 'case_study', 'sectors' => $sector->slug, 'posts_per_page' => -1 , 'order' => 'ASC', 'orderby' => 'title'));
+										query_posts(array('post_type' => 'case_study', 'sectors' => 'financial', 'posts_per_page' => -1 , 'order' => 'ASC', 'orderby' => 'title'));
 										while(have_posts()) : the_post();  ?>
 
 										<li>
 											<a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
-												<?php the_title(); ?> (<?php echo $sector->slug ?>
+												<?php the_title(); ?> (<?php echo $sector->slug ?>)
 											</a>
 										</li>
 
@@ -36,9 +36,14 @@
 										
 										
 										<?php 
-										$query = new WP_Query( 'post_type=any', array( 'sectors' => 'health' ) ); 
-										print_r($query);
-											
+										$query = new WP_Query( 'post_type=case_study', array( 'sectors' => 'financial' ) ); 
+										if( $query->have_posts() ) {
+											echo 'List of '.$post_type . ' where the taxonomy '. $tax . '  is '. $tax_term->name;
+											while ($query->have_posts()) : $query->the_post(); ?>
+												<p><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
+											<?php endwhile;
+										}
+										wp_reset_query();
 										?>
 										
 										
