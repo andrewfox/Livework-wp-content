199342477135f11d285da3fdd98bceec33fb7e86
Dom <dom@livework.co.uk>
2012-12-14 16:38:02 +0000

    .

--- themes/livework/loop.php
+++ themes/livework/loop.php
@@ -14,25 +14,25 @@
  * <code>get_template_part( 'loop', 'index' );</code>
  *
  * @package WordPress
- * @subpackage Boilerplate
- * @since Boilerplate 1.0
+ * @subpackage livework
+ * @since livework 1.0
  */
 ?>
 
 <?php /* Display navigation to next/previous pages when applicable */ ?>
 <?php if ( $wp_query->max_num_pages > 1 ) : ?>
 	<nav id="nav-above" class="navigation">
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'boilerplate' ) ); ?></div>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'boilerplate' ) ); ?></div>
+		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'livework' ) ); ?></div>
+		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'livework' ) ); ?></div>
 	</nav><!-- #nav-above -->
 <?php endif; ?>
 
 <?php /* If there are no posts to display, such as an empty archive page */ ?>
 <?php if ( ! have_posts() ) : ?>
 	<article id="post-0" class="post error404 not-found">
-		<h1 class="entry-title"><?php _e( 'Not Found', 'boilerplate' ); ?></h1>
+		<h1 class="entry-title"><?php _e( 'Not Found', 'livework' ); ?></h1>
 		<div class="entry-content">
-			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'boilerplate' ); ?></p>
+			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'livework' ); ?></p>
 			<?php get_search_form(); ?>
 		</div><!-- .entry-content -->
 	</article><!-- #post-0 -->
@@ -57,12 +57,12 @@
 
 <?php /* How to display posts in the Gallery category. */ ?>
 
-	<?php if ( in_category( _x('gallery', 'gallery category slug', 'boilerplate') ) ) : ?>
+	<?php if ( in_category( _x('gallery', 'gallery category slug', 'livework') ) ) : ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'boilerplate' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'livework' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 			<div class="entry-meta">
-				<?php boilerplate_posted_on(); ?>
+				<?php livework_posted_on(); ?>
 			</div><!-- .entry-meta -->
 
 			<div class="entry-content">
@@ -79,8 +79,8 @@
 						<div class="gallery-thumb">
 							<a class="size-thumbnail" href="<?php the_permalink(); ?>"><?php echo $image_img_tag; ?></a>
 						</div><!-- .gallery-thumb -->
-						<p><em><?php printf( __( 'This gallery contains <a %1$s>%2$s photos</a>.', 'boilerplate' ),
-								'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'boilerplate' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
+						<p><em><?php printf( __( 'This gallery contains <a %1$s>%2$s photos</a>.', 'livework' ),
+								'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'livework' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
 								$total_images
 							); ?></em></p>
 				<?php endif; ?>
@@ -89,16 +89,16 @@
 			</div><!-- .entry-content -->
 
 			<footer class="entry-utility">
-				<a href="<?php echo get_term_link( _x('gallery', 'gallery category slug', 'boilerplate'), 'category' ); ?>" title="<?php esc_attr_e( 'View posts in the Gallery category', 'boilerplate' ); ?>"><?php _e( 'More Galleries', 'boilerplate' ); ?></a>
+				<a href="<?php echo get_term_link( _x('gallery', 'gallery category slug', 'livework'), 'category' ); ?>" title="<?php esc_attr_e( 'View posts in the Gallery category', 'livework' ); ?>"><?php _e( 'More Galleries', 'livework' ); ?></a>
 				|
-				<?php comments_popup_link( __( 'Leave a comment', 'boilerplate' ), __( '1 Comment', 'boilerplate' ), __( '% Comments', 'boilerplate' ) ); ?>
-				<?php edit_post_link( __( 'Edit', 'boilerplate' ), '|', '' ); ?>
+				<?php comments_popup_link( __( 'Leave a comment', 'livework' ), __( '1 Comment', 'livework' ), __( '% Comments', 'livework' ) ); ?>
+				<?php edit_post_link( __( 'Edit', 'livework' ), '|', '' ); ?>
 			</footer><!-- .entry-utility -->
 		</article><!-- #post-## -->
 
 <?php /* How to display posts in the asides category */ ?>
 
-	<?php elseif ( in_category( _x('asides', 'asides category slug', 'boilerplate') ) ) : ?>
+	<?php elseif ( in_category( _x('asides', 'asides category slug', 'livework') ) ) : ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 		<?php if ( is_archive() || is_search() ) : // Display excerpts for archives and search. ?>
@@ -107,15 +107,15 @@
 			</div><!-- .entry-summary -->
 		<?php else : ?>
 			<div class="entry-content">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'boilerplate' ) ); ?>
+				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'livework' ) ); ?>
 			</div><!-- .entry-content -->
 		<?php endif; ?>
 
 			<footer class="entry-utility">
-				<?php boilerplate_posted_on(); ?>
+				<?php livework_posted_on(); ?>
 				|
-				<?php comments_popup_link( __( 'Leave a comment', 'boilerplate' ), __( '1 Comment', 'boilerplate' ), __( '% Comments', 'boilerplate' ) ); ?>
-				<?php edit_post_link( __( 'Edit', 'boilerplate' ), '| ', '' ); ?>
+				<?php comments_popup_link( __( 'Leave a comment', 'livework' ), __( '1 Comment', 'livework' ), __( '% Comments', 'livework' ) ); ?>
+				<?php edit_post_link( __( 'Edit', 'livework' ), '| ', '' ); ?>
 			</footer><!-- .entry-utility -->
 		</article><!-- #post-## -->
 
@@ -123,10 +123,10 @@
 
 	<?php else : ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'boilerplate' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'livework' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 			<div class="entry-meta">
-				<?php boilerplate_posted_on(); ?>
+				<?php livework_posted_on(); ?>
 			</div><!-- .entry-meta -->
 
 	<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
@@ -135,25 +135,25 @@
 			</div><!-- .entry-summary -->
 	<?php else : ?>
 			<div class="entry-content">
-				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'boilerplate' ) ); ?>
-				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'boilerplate' ), 'after' => '</div>' ) ); ?>
+				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'livework' ) ); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'livework' ), 'after' => '</div>' ) ); ?>
 			</div><!-- .entry-content -->
 	<?php endif; ?>
 
 			<footer class="entry-utility">
 				<?php if ( count( get_the_category() ) ) : ?>
-					<?php printf( __( 'Posted in %2$s', 'boilerplate' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
+					<?php printf( __( 'Posted in %2$s', 'livework' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
 					|
 				<?php endif; ?>
 				<?php
 					$tags_list = get_the_tag_list( '', ', ' );
 					if ( $tags_list ):
 				?>
-					<?php printf( __( 'Tagged %2$s', 'boilerplate' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+					<?php printf( __( 'Tagged %2$s', 'livework' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
 					|
 				<?php endif; ?>
-				<?php comments_popup_link( __( 'Leave a comment', 'boilerplate' ), __( '1 Comment', 'boilerplate' ), __( '% Comments', 'boilerplate' ) ); ?>
-				<?php edit_post_link( __( 'Edit', 'boilerplate' ), '| ', '' ); ?>
+				<?php comments_popup_link( __( 'Leave a comment', 'livework' ), __( '1 Comment', 'livework' ), __( '% Comments', 'livework' ) ); ?>
+				<?php edit_post_link( __( 'Edit', 'livework' ), '| ', '' ); ?>
 			</footer><!-- .entry-utility -->
 		</article><!-- #post-## -->
 
@@ -166,7 +166,7 @@
 <?php /* Display navigation to next/previous pages when applicable */ ?>
 <?php if (  $wp_query->max_num_pages > 1 ) : ?>
 	<nav id="nav-below" class="navigation">
-		<?php next_posts_link( __( '&larr; Older posts', 'boilerplate' ) ); ?>
-		<?php previous_posts_link( __( 'Newer posts &rarr;', 'boilerplate' ) ); ?>
+		<?php next_posts_link( __( '&larr; Older posts', 'livework' ) ); ?>
+		<?php previous_posts_link( __( 'Newer posts &rarr;', 'livework' ) ); ?>
 	</nav><!-- #nav-below -->
 <?php endif; ?>
