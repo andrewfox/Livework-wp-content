336571b48e2e60a04c2d3bb4c71edd4061f1fd7e
Dom <dom@livework.co.uk>
2012-12-14 16:37:14 +0000

    .

--- themes/livework/category.php
+++ themes/livework/category.php
@@ -21,9 +21,9 @@ get_header(); ?>
 				 * If you want to overload this in a child theme then include a file
 				 * called loop-category.php and that will be used instead.
 				 */
-				get_template_part( 'index' );
+				get_template_part( 'loop', 'category' );
 				?>
-				<p>Hello there!2</p>
+				<p>Hello there!</p>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
