ac7d4b38992e32b59d760954e17884ce7d98d0f8
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-14 13:56:18 +0000

    .

--- themes/livework/header.php
+++ themes/livework/header.php
@@ -83,7 +83,8 @@
 
 			<?php 
 			// Our Client's Stories Sector menu
-			if (is_tree(9)) || get_post_type( $post->ID ) == 'case_study' ) { 
+			$posttype = get_post_type( $post->ID );
+			if (is_tree(9)) || $posttype == 'case_study' ) { 
 			?>
 			<nav id="menu-secondary" class="nav-sectors clearfix">
 				<?php wp_nav_menu( array('menu' => 'Sectors', 'container' => '' )); ?>
