269a636792fc4923f39357583167fdb9740a2e22
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-15 08:43:51 +0000

    .

--- themes/livework/sidebar-archives.php
+++ themes/livework/sidebar-archives.php
@@ -11,6 +11,6 @@
 
 					<div class="archives">
 						<ul class="list-archive-cat">
-							<?php wp_list_categories('order=ASC&orderby=count&show_count=0&title_li='); ?>
+							<?php wp_list_categories('order=DESC&orderby=count&show_count=0&title_li='); ?>
 						</ul>
 					</div>
