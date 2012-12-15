65586931e64143b528f0ec51070fe576b108f400
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-15 22:08:11 +0000

    .

--- themes/livework/footer.php
+++ themes/livework/footer.php
@@ -41,7 +41,7 @@
 
 		<footer role="contentinfo">
 
-			<?php echo date("Y"); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			<p>&copy; <?php echo date("Y"); ?> <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			
 			<nav id="social">
 				<?php wp_nav_menu( array('container' => '', 'menu' => 'Footer' )); ?>
