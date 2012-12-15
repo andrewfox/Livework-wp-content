7eb2c6962bdab4f5b76af8764ac5a910ba803bb3
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-15 10:47:32 +0000

    .

--- themes/livework/sidebar-archives-date.php
+++ themes/livework/sidebar-archives-date.php
@@ -11,6 +11,8 @@
 
 					<div class="archives clearfix">
 						<ul class="list-archive-date">
+							<li>Date archives</li>
 							<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 						</ul>
+						<div class="expander">See all dates &rarr;</div>
 					</div>
