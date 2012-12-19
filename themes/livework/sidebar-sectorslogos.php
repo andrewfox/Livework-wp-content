8265b4a6500600ed5a873e5a80a11a1bf5bfe022
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-19 12:30:33 +0000

    .

--- themes/livework/sidebar-sectorslogos.php
+++ themes/livework/sidebar-sectorslogos.php
@@ -33,11 +33,11 @@
 									while ( $the_query->have_posts() ) : $the_query->the_post(); 
 									?>
 									<li>
-										<?php if ( in_category('logo-only-case-study') && get_field('casestudies_logo') ) : ?>
+										<?php if ( in_category('logo-only-case-study') && get_field('casestudies_logo') ) { ?>
 										<img src="<?php the_field('casestudies_logo'); ?>" alt="<?php the_title(); ?>" />
-										<?php elseif ( get_field('casestudies_logo') : ?>
+										<?php } elseif ( get_field('casestudies_logo') { ?>
 										<a href="<?php the_permalink() ?>" rel="bookmark" title="Client story <?php the_title_attribute(); ?>"><img src="<?php the_field('casestudies_logo'); ?>" alt="<?php the_title(); ?>" /></a>
-										<?php endif; ?>
+										<?php } ?>
 									</li>
 									<?php endwhile; ?>
 									<?php wp_reset_postdata() ?>
