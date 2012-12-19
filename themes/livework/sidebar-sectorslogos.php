8a94ac35d2a7d4774f408ef757edb0a37ba8d5e7
Andrew Fox <andrew@andrewfox.co.uk>
2012-12-19 12:50:08 +0000

    .

--- themes/livework/sidebar-sectorslogos.php
+++ themes/livework/sidebar-sectorslogos.php
@@ -32,13 +32,11 @@
 									$the_query = new WP_Query( $args );
 									while ( $the_query->have_posts() ) : $the_query->the_post(); 
 									?>
-									<li>
-										<?php if ( in_category('logo-only-case-study') && get_field('casestudies_logo') ) : ?>
-										<img src="<?php the_field('casestudies_logo'); ?>" alt="<?php the_title(); ?>" /><?php the_title(); ?>
-										<?php elseif ( get_field('casestudies_logo')) : ?>
-										<a href="<?php the_permalink() ?>" rel="bookmark" title="Client story <?php the_title_attribute(); ?>"><img src="<?php the_field('casestudies_logo'); ?>" alt="<?php the_title(); ?>" /></a><?php the_title(); ?>
-										<?php endif; ?>
-									</li>
+									<?php if ( in_category('logo-only-case-study') && get_field('casestudies_logo') ) : ?>
+									<li><img src="<?php the_field('casestudies_logo'); ?>" alt="<?php the_title(); ?>" /><?php the_title(); ?></li>
+									<?php elseif ( get_field('casestudies_logo')) : ?>
+									<li><a href="<?php the_permalink() ?>" rel="bookmark" title="Client story <?php the_title_attribute(); ?>"><img src="<?php the_field('casestudies_logo'); ?>" alt="<?php the_title(); ?>" /></a><?php the_title(); ?></li>
+									<?php endif; ?>
 									<?php endwhile; ?>
 									<?php wp_reset_postdata() ?>
 								</ul>
