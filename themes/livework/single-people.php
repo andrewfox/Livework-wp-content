099011e53d99102ff66795b30ad2680d7b7d53e8
Dom <dom@livework.co.uk>
2013-01-02 15:31:37 +0000

    .

--- themes/livework/single-people.php
+++ themes/livework/single-people.php
@@ -67,7 +67,7 @@ get_header(); ?>
 									
 										<?php 
 										
-										if ( get_field('person_email') = "" ) {
+										if ( get_field('person_email') == "" ) {
 											
 										}
 										
