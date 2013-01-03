177f18146d8206b4182d5da2dc3ea92c6781d99d
Dom <dom@livework.co.uk>
2013-01-03 15:22:42 +0000

    loop change no posts statement

--- themes/livework/loop.php
+++ themes/livework/loop.php
@@ -21,16 +21,7 @@
 
 
 
-<?php /* If there are no posts to display, such as an empty archive page */ ?>
-<?php if ( ! have_posts() ) : ?>
-	<article id="post-0" class="post error404 not-found">
-		<h1 class="entry-title"><?php _e( 'Not Found', 'livework' ); ?></h1>
-		<div class="entry-content">
-			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'livework' ); ?></p>
-			<?php get_search_form(); ?>
-		</div><!-- .entry-content -->
-	</article><!-- #post-0 -->
-<?php endif; ?>
+
 
 <?php
 	/* Start the Loop.
@@ -136,10 +127,9 @@ endif; ?>
 						</div><!-- /.post -->
 						
 	
-						<?php 
-						endwhile; 
-						endif;
-						?>
+						<?php endwhile; else: ?>
+						 <p>Sorry, no posts matched your criteria.</p>
+						 <?php endif; ?>
 	
 	
 	<?php /* Display navigation to next/previous pages when applicable */ ?>
