33c3cc6765d329d001055519643df0732ea1d51f
Dom <dom@livework.co.uk>
2013-01-03 15:04:06 +0000

    index remove get part index

--- themes/livework/index.php
+++ themes/livework/index.php
@@ -28,9 +28,9 @@ get_header(); ?>
 <?php
 /* Run the loop to output the posts.
  * If you want to overload this in a child theme then include a file
- * called loop-index.php and that will be used instead.
+ * called loop-index.php and that will be used instead. 
  */
- get_template_part( 'loop', 'index' );
+ get_template_part( 'loop' );
 ?>
 
 
