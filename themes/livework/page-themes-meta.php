45842fbd9331508de821f8034e50fc0e9e61993c
Dom <dom@livework.co.uk>
2013-01-03 11:56:04 +0000

    add themes-meta

---
+++ themes/livework/page-themes-meta.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Template Name: Page - Sector Meta
+ * Description: Our Client's Stories Sector page: fullscreen area, list of case studies and themes
+ *
+ * @package WordPress
+ * @subpackage Livework
+ * @since Boilerplate 1.0
+ */
+
+get_header(); ?>
+
+
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<div class="splash alt <?php the_field('colour'); ?>">
+					<div class="topimage"><?php the_post_thumbnail('large'); ?></div>
+					<div id="introduction" class="alt">
+						<div class="wrapper">
+							<h1><?php the_title(); ?></h1>
+							<div class="excerpt">
+								<?php the_content() ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<?php endwhile; ?>
+
+				<article id="page-<?php the_ID(); ?>" class="main">
+
+
+
+
+				<?php 
+				// Featured bar
+				get_sidebar('featuredbar'); 
+				?>
+
+				<?php 
+				// Sectors + clients listings
+//				get_sidebar('sectorslogos'); 
+				?>
+				
+				<?php 
+				// Sectors + clients listings
+//				get_sidebar('sectorsclients'); 
+				?>
+
+
+
+
+	 			</article> <!-- /.main -->
+
+
+
+
+
+
+
+
+
+
+	<?php get_footer(); ?>
