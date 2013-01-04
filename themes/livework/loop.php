11d543f53272226528c3ea0d0a2f986dda53862b
Dom <dom@livework.co.uk>
2013-01-04 15:35:00 +0000

    .

--- themes/livework/loop.php
+++ themes/livework/loop.php
@@ -49,14 +49,16 @@ if ((get_post_type( $post->ID ) == "case_study")) : ?>
 								</div>
 								<div class="case-studies-content">
 									
-									<h4 class="tab">Client story</h4>
+									
 									
 									<?php if( get_field('casestudies_one_liner') ): ?>
 										<h2>
 											
 										<?php if ( in_category(196) ) : ?>
+												<h4 class="tab">Short story</h4>
 												<?php the_field('casestudies_one_liner'); ?> <span class="casestudy-title">with <span><?php the_title(); ?></span></span>
 										<?php else : ?>
+											<h4 class="tab">Client story</h4>
 										<a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark">
 												<?php the_field('casestudies_one_liner'); ?> <span class="casestudy-title">with <span><?php the_title(); ?></span></span>
 											</a>
