40eb9e7f9e692176c467d755a53a4c83fce5303c
Dom <dom@livework.co.uk>
2013-01-04 11:40:56 +0000

    .

--- themes/livework/js/main.js
+++ themes/livework/js/main.js
@@ -222,9 +222,14 @@ function rescale() {
 	
 };
 
-map = new google.maps.Map(document.getElementById("map_canvas"), {
-      panControl: false,
-      zoomControl: false,
-      scaleControl: false,
- });
+window.onload = function() {
+    var myOptions = {
+        center: new google.maps.LatLng(40.744556,-73.987378),
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+    };
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
 
