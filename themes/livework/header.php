801f6770e0e7b9ef64c341547de43eb891bb7958
Andrew Fox <andrew@andrewfox.co.uk>
2013-01-04 15:26:40 +0000

    respond.js
    Makes media queries work for <IE8

--- themes/livework/header.php
+++ themes/livework/header.php
@@ -41,6 +41,7 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="<?php bloginfo( 'template_directory' ); ?>/js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
 		<script src="<?php echo bloginfo('template_directory'); ?>/js/vendor/modernizr-2.6.2.min.js"></script>
+		<script src="<?php echo bloginfo('template_directory'); ?>/js/vendor/respond.min.js"></script>
 		<script src="<?php echo bloginfo('template_directory'); ?>/js/vendor/jquery.lightbox-0.5.min.js"></script>
 		<script src="<?php bloginfo( 'template_directory' ); ?>/js/main.js"></script>
 
