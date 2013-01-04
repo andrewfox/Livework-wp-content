75187b431e8f1e979646da17171411bf047c32a1
Dom <dom@livework.co.uk>
2013-01-04 11:23:54 +0000

    .

--- themes/livework/page-studio.php
+++ themes/livework/page-studio.php
@@ -40,13 +40,15 @@ get_header(); ?>
 						<aside class="entry-data"> 
 
 							<div class="vcard">
-								<?php if ( get_field('studio_google_maps') ) : ?><div class="studio-map"> <?php the_field('studio_google_maps'); ?><?php endif; ?>						
+														
 								<?php if ( get_field('studio_address') ) : ?><p class="studio-address"><span class="ssstandard">location</span> <?php the_field('studio_address'); ?></p><?php endif; ?>
 								<?php if ( get_field('studio_telephone') ) : ?><p class="tel"><span class="ssstandard">call</span> <?php the_field('studio_telephone'); ?></p><?php endif; ?>
 								<?php if ( get_field('studio_fax') ) : ?><p class="fax"><span class="ssstandard">fax</span> <?php the_field('studio_fax'); ?></p><?php endif; ?>
 								<?php if ( get_field('studio_email') ) : ?><p class="email"><span class="ssstandard">email</span> <a href="mailto:<?php the_field('studio_email'); ?>"><?php the_field('studio_email'); ?></a></p><?php endif; ?>
 								<?php if ( get_field('studio_twitter') ) : ?><p class="studio-twitter"><span class="sssocial">twitter</span> <a href="http://twitter.com/<?php the_field('studio_twitter'); ?>">@<?php the_field('studio_twitter'); ?></a></p><?php endif; ?>
 								<?php if ( get_field('studio_facebook') ) : ?><p class="studio-facebook"><span class="sssocial">facebook</span> <a href="http://facebook.com/<?php the_field('studio_facebook'); ?>">Facebook</a></p><?php endif; ?>
+								
+								<?php if ( get_field('studio_google_maps') ) : ?><div class="studio-map"> <?php the_field('studio_google_maps'); ?><?php endif; ?>
 
 							</div>
 
