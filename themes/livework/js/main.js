6ddfd91787a3f1651713cbe4c8c2e27a8548efbd
Dom <dom@livework.co.uk>
2013-01-07 14:03:46 +0000

    maps js

--- themes/livework/js/main.js
+++ themes/livework/js/main.js
@@ -222,14 +222,14 @@ function rescale() {
 	
 };
 
-window.onload = function() {
-    var myOptions = {
-        center: new google.maps.LatLng(40.744556,-73.987378),
-        zoom: 18,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true
-    };
-
-    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-}
+//window.onload = function() {
+//    var myOptions = {
+//        center: new google.maps.LatLng(40.744556,-73.987378),
+//        zoom: 18,
+//        mapTypeId: google.maps.MapTypeId.ROADMAP,
+//        disableDefaultUI: true
+//    };
+//
+//    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+//}
 
