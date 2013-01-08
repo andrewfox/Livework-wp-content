ddf7e78f3957b62d4cf4f2376aa2e0f4ec05a5e3
Dom <dom@livework.co.uk>
2013-01-08 12:20:36 +0000

    .

--- themes/livework/sidebar-list-sectors.php
+++ themes/livework/sidebar-list-sectors.php
@@ -12,13 +12,11 @@
 				
 							<?php query_posts(array('showposts' => -1, 'post_parent' => 9, 'post_type' => 'page', 'order' => 'asc', )); while (have_posts()) { the_post(); ?>
 				
-							<div class="list-of-pages">
 				
 								<h4><a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
 				
 								
 				
-							</div> 
 					
 							<?php } ?>
 							<?php wp_reset_query();?>
