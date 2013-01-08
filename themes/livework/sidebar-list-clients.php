850508d703b919d1b0a856fb1ca1ab120eb47eb0
Dom <dom@livework.co.uk>
2013-01-08 12:10:52 +0000

    .

---
+++ themes/livework/sidebar-list-clients.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The Sidebar containing list of sectors + clients
+ *
+ * @package WordPress
+ * @subpackage Livework
+ * @since Boilerplate 1.0
+ */
+?>
+
+
+
+					
+						
+				<div class="list-of-pages">
+
+								<ul>
+									<?php $args = array(
+										'post_type'=> 'case_study',
+										'taxonomy' => 'sectors',
+										'showposts' => 10,
+//										'term' => $sector->slug,
+//										'orderby' => 'title',
+//										'order' => 'ASC'
+									);
+						
+									$the_query = new WP_Query( $args );
+									while ( $the_query->have_posts() ) : $the_query->the_post(); 
+									?>
+									<li>
+										<?php if (in_category('logo-only-case-study')) : ?>
+										<?php the_title(); ?>
+										<?php else: ?>
+										<a href="<?php the_permalink() ?>" rel="bookmark" title="Client story <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+										<?php endif; ?>
+									</li>
+									<?php endwhile; ?>
+									<?php wp_reset_postdata() ?>
+								</ul>
+							</li>
+						</ul>
+						<?php } ?>
+					<?php } ?>
+
+
+
+
+				</div><!-- /.sectors-clients-list -->
--- themes/livework/sidebar-list-themes.php
+++ themes/livework/sidebar-list-themes.php
@@ -12,7 +12,7 @@
 				
 							<?php query_posts(array('showposts' => -1, 'post_parent' => 2500, 'post_type' => 'page', 'order' => 'asc', )); while (have_posts()) { the_post(); ?>
 				
-							<div class="">
+							<div class="list-of-pages">
 				
 								<h2><a href="<?php the_permalink(); ?>" title="<?php printf( __('Read', 'blankslate'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 				
