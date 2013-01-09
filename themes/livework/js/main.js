7c7dc25f1c281717b91e77fec489e5db6409d637
Dom <dom@livework.co.uk>
2013-01-09 17:26:16 +0000

    /

--- themes/livework/js/main.js
+++ themes/livework/js/main.js
@@ -72,7 +72,7 @@ $(function() {
 		}
 		
 		if ($hellothere == 6) {
-//			$('nav#access h1 a img').attr('src','http://www.liveworkstudio.com/wordpress/wp-content/themes/livework/img/hellologo.png');
+			$('nav#access h1 a img').attr('src','http://www.liveworkstudio.com/wordpress/wp-content/themes/livework/img/hellologo.png');
 		}
 
 	});
